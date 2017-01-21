c7cdbd33d4583e5d02fe3405380123420e99a10c
Jinyue Song <jinyuesong88@gmail.com>
2017-01-20 23:59:29 -0800

    initial structure

---
+++ cp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* check properties */
+
+checkProperty(int F){
+	int wantedProperty = F;
+
+	switch (wantedProperty){
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+	}
+
+return 0;
+}
